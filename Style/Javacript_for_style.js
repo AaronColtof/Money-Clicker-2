ce71e1072d75c64269bc61b313fc2fea574b1b61
KaikouFox <kaikou.van.gemerden@coderclass.nl>
2022-04-13 17:49:52 +0200

    button click added

---
+++ Style/Javacript_for_style.js
@@ -0,0 +1,5 @@
+let button_click = document.getElementById("moneyButton").addEventListener("click", clickAlert);
+
+function clickAlert(){
+    document.getElementById("img_clicker-button").style.width = "240px";
+}
