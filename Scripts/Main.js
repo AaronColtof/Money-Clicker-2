caa886900391eaacf2d829c3f16cc8fd49bd7cde
AaronColtof <Aaron.Coltof@gmail.com>
2022-04-13 17:05:51 +0200

    Text toegevoegd aan de HTML-File en js file bijgewerkt

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,8 +1,10 @@
 let currentMoney = 100;
-let totalMoneyEarned = 0;
-let botCPS = 10;
+let totalMoneyEarned = 100;
+let botCPS = 0;
 
 let currentMoneyText = document.getElementById("moneyText");
+let botCPSText = document.getElementById("botCPSText");
+let totalMoneyText = document.getElementById("totalMoney");
 
 function addMoney(moneyToAdd){
     console.log(moneyToAdd);
@@ -14,7 +16,9 @@ function subtractMoney(moneyToSubtract){
 }
 
 function updateMoneyText(){
-    currentMoneyText.innerHTML = currentMoney;
+    currentMoneyText.innerHTML = "Money: " + currentMoney;
+    botCPSText.innerHTML = "CPS: " + botCPS; 
+    totalMoneyText.innerHTML = "Total: " + totalMoneyEarned; 
 }
 
 function addToTotalMoneyEarned(moneyAdded)
